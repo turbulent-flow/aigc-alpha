6bc503a3b36b16e0013ccfcdac8d0cfa4ef1364a
liam <hello@mengliu.dev>
2025-06-09 15:16:24 +0800

    fix: no function clause matching

--- lib/aigc_alpha/core/template.ex
+++ lib/aigc_alpha/core/template.ex
@@ -1,6 +1,7 @@
 defmodule AIGCAlpha.Core.Template do
   @derive Jason.Encoder
-  defstruct type: :translate_to_en, content: "请把大括号中的中文翻译成英文，{xxxxx}。"
+  defstruct type: :translate_to_en,
+            content: "请把大括号中的中文翻译成英文，{xxxxx}，注意直接输出翻译的内容，例如：不要把翻译的内容放在大括号内。"
 
   @type t :: %__MODULE__{
           type: atom(),
